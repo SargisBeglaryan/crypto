93adc5e487a4148ace43d7ac8203d1f7c1a5a96b
Sargis <sargisbelaryan1989@gmail.com>
2018-10-12 20:18:36 +0400

    Create  Guide block.

--- index.php
+++ index.php
@@ -67,11 +67,11 @@
 			</div>
 			<div class="col-sm-6 col-md-4 col-lg-2 text-center">
 				<div class="currencyDetailsContent mb-3 active">
-					<a href="#" class="text-dark">
+					<div class="text-dark">
 						<strong>EUR/USD</strong>
 						<strong>0.90029</strong>
 						<span>00000</span>
-					</a>
+					</div>
 				</div>
 			</div>
 			<div class="col-sm-6 col-md-4 col-lg-2 text-center">
@@ -95,7 +95,7 @@
 		</div>
 	</div>
 	<div class="container-fluid mt-4 mb-4">
-		<div class="row mt-3">
+		<div class="row">
 			<div class="col-sm-12 text-left">
 				<h3 class="rate-title mt-1 mb-3">Exchange rate in the last 30 days</h3>
 				<p class="rate-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -109,7 +109,7 @@
 		</div>
 	</div>
 	<div class="container-fluid mt-5 mb-5">
-		<div class="row mt-3">
+		<div class="row">
 			<div class="col-sm-6 p-0 mb-3">
 				<div class="currency-info-header text-center pb-4 pt-4">
 					<div class="currency-header-block">
@@ -152,6 +152,66 @@
 							Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
 						</p>
 					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid mt-5 mb-5">
+		<div class="row">
+			<div class="col-sm-12 mb-3">
+				<div class="guide-title d-inline-block text-left float-left mt-sm-2 mt-md-0 mt-2">
+					Quick Conversion Guide
+				</div>
+				<button type="button" class="btn btn-primary float-right">Print
+					<span class="ml-2 icon-printer"></span>
+				</button>
+			</div>
+			<div class="col-sm-6 mb-3">
+				<div class="bg-dark text-center p-2 text-light">display From (EUR) Euros to (USD) Dollars
+				</div>
+				<div class="col p-0 d-flex guide-columns">
+					<div class="col-6 d-inline-block text-right pt-2 pb-2 pl-0 pr-2 bg-grey"><span class="pr-2 icon-euro"></span>0.85 EUR</div>
+					<div class="col-6 d-inline-block guide-column-first text-left pt-2 pb-2 pl-2 pr-0">1 USD</div>
+				</div>
+				<div class="col p-0 d-flex guide-columns">
+					<div class="col-6 d-inline-block text-right pt-2 pb-2 pl-0 pr-2 bg-grey"><span class="pr-2 icon-euro"></span>0.85 EUR</div>
+					<div class="col-6 d-inline-block guide-column-first text-left pt-2 pb-2 pl-2 pr-0">1 USD</div>
+				</div>
+				<div class="col p-0 d-flex guide-columns">
+					<div class="col-6 d-inline-block text-right pt-2 pb-2 pl-0 pr-2 bg-grey"><span class="pr-2 icon-euro"></span>0.85 EUR</div>
+					<div class="col-6 d-inline-block guide-column-first text-left pt-2 pb-2 pl-2 pr-0">1 USD</div>
+				</div>
+				<div class="col p-0 d-flex guide-columns">
+					<div class="col-6 d-inline-block text-right pt-2 pb-2 pl-0 pr-2 bg-grey"><span class="pr-2 icon-euro"></span>0.85 EUR</div>
+					<div class="col-6 d-inline-block guide-column-first text-left pt-2 pb-2 pl-2 pr-0">1 USD</div>
+				</div>
+				<div class="col p-0 d-flex guide-columns">
+					<div class="col-6 d-inline-block text-right pt-2 pb-2 pl-0 pr-2 bg-grey"><span class="pr-2 icon-euro"></span>0.85 EUR</div>
+					<div class="col-6 d-inline-block guide-column-first text-left pt-2 pb-2 pl-2 pr-0">1 USD</div>
+				</div>
+			</div>
+			<div class="col-sm-6 mb-3">
+				<div class="bg-dark text-center p-2 text-light">display From (USD) Dollars to (EUR) Euros
+				</div>
+				<div class="col p-0 d-flex guide-columns">
+					<div class="col-6 d-inline-block guide-column-first text-right pt-2 pb-2 pl-0 pr-2 bg-grey">1 USD</div>
+					<div class="col-6 d-inline-block text-left pt-2 pb-2 pl-2 pr-0"><span class="pr-2 icon-euro"></span>0.85 EUR</div>
+				</div>
+				<div class="col p-0 d-flex guide-columns">
+					<div class="col-6 d-inline-block guide-column-first text-right pt-2 pb-2 pl-0 pr-2 bg-grey">1 USD</div>
+					<div class="col-6 d-inline-block text-left pt-2 pb-2 pl-2 pr-0"><span class="pr-2 icon-euro"></span>0.85 EUR</div>
+				</div>
+				<div class="col p-0 d-flex guide-columns">
+					<div class="col-6 d-inline-block guide-column-first text-right pt-2 pb-2 pl-0 pr-2 bg-grey">1 USD</div>
+					<div class="col-6 d-inline-block text-left pt-2 pb-2 pl-2 pr-0"><span class="pr-2 icon-euro"></span>0.85 EUR</div>
+				</div>
+				<div class="col p-0 d-flex guide-columns">
+					<div class="col-6 d-inline-block guide-column-first text-right pt-2 pb-2 pl-0 pr-2 bg-grey">1 USD</div>
+					<div class="col-6 d-inline-block text-left pt-2 pb-2 pl-2 pr-0"><span class="pr-2 icon-euro"></span>0.85 EUR</div>
+				</div>
+				<div class="col p-0 d-flex guide-columns">
+					<div class="col-6 d-inline-block guide-column-first text-right pt-2 pb-2 pl-0 pr-2 bg-grey">1 USD</div>
+					<div class="col-6 d-inline-block text-left pt-2 pb-2 pl-2 pr-0"><span class="pr-2 icon-euro"></span>0.85 EUR</div>
 				</div>
 			</div>
 		</div>
