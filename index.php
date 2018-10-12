299278d245fb74c3a825be3d2108dfd910c82919
Sargis <sargisbelaryan1989@gmail.com>
2018-10-12 17:32:06 +0400

    Build dollar-euro block.

--- index.php
+++ index.php
@@ -93,7 +93,68 @@
 				</div>
 			</div>
 		</div>
+	</div>
+	<div class="container-fluid mt-4 mb-4">
+		<div class="row mt-3">
+			<div class="col-sm-12 text-left">
+				<h3 class="rate-title mt-1 mb-3">Exchange rate in the last 30 days</h3>
+				<p class="rate-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col-sm-12 text-left">
+				<h3 class="rate-title mt-1 mb-3">Exchange rate in the last 30 days</h3>
+				<p class="rate-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid mt-5 mb-5">
+		<div class="row mt-3">
+			<div class="col-sm-6 p-0 mb-3">
+				<div class="currency-info-header text-center pb-4 pt-4">
+					<div class="currency-header-block">
+						<span class="d-block currency-icon icon-euro"></span>
+						<span class="d-block currency-name">Euro</span>
+					</div>
+				</div>
+				<div class="currency-info-content  pt-4 pl-3 text-left">
+					<div class="currency-description-content line pr-4">
+						<p class="currency-info-details">
+							<strong class="d-block">Country: United States</strong>
+							<strong class="d-block">Cod: 3129 3019 2823</strong>
+						</p>
+						<p class="currency-content-description">
+							<strong class="d-block">Description</strong>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><br>
 
+							Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 p-0">
+				<div class="currency-info-header text-center">
+					<div class="pb-4 pt-4">
+						<span class="d-block currency-icon icon-dollar"></span>
+						<span class="d-block currency-name">Dollar</span>
+					</div>
+				</div>
+				<div class="currency-info-content  pt-4 pl-3 text-left">
+					<div class="currency-description-content pr-4 pl-sm-3">
+						<p class="currency-info-details">
+							<strong class="d-block">Country: United States</strong>
+							<strong class="d-block">Cod: 3129 3019 2823</strong>
+						</p>
+						<p class="currency-content-description">
+							<strong class="d-block">Description</strong>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><br>
+
+							Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 
 <script src="js/scripts.js"></script>
