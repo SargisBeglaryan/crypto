dcc38ba76f503fb827680ea9f12847839a4b69f0
Sargis <sargisbelaryan1989@gmail.com>
2018-10-17 17:08:51 +0400

    Add functionality for search .

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,16 @@ $(document).ready(function () {
 
 	$("#search-input").on('focusin', function (event) {
         $('.search-form-content').addClass('widtAnimation');
+        if($(window).width() < 768 && $(this).val() != '') {
+			$('.search-result').fadeIn(100);
+        }
     });
 
     $("#search-input").on('focusout', function (event) {
         $('.search-form-content').removeClass('widtAnimation');
+        if($(window).width() < 768) {
+			$('.search-result').fadeOut(100);
+        }
     });
 
     $('.print').on('click', function() {
@@ -30,6 +36,57 @@ $(document).ready(function () {
 	$('.menu-footer-button').on('click', function () {
 		$('footer').fadeIn(200);
 		$('.menu-footer').fadeOut(200);
+	});
+
+	$('#search-input').on('keyup', function(){
+
+		var searchWord = $(this).val().trim();
+		if(searchWord == '') {
+			$('.search-result').fadeOut(100);
+			return false;
+		}
+
+		/* this functionality need to removed on future */
+		var findedData = 0;
+		$('.listSearch a').each(function (){
+			if($(this).text().toLowerCase().indexOf(searchWord) >= 0) {
+				$(this).show();
+				findedData ++;
+			} else {
+				$(this).hide();
+			}
+		});
+		if(findedData == 0) {
+			$('.no-result').show();
+		} else {
+			$('.search-result').fadeIn(100);
+			$('.no-result').hide();
+		}
+
+		/* above functionality need to removed*/
+
+		/* ajax call need to uncommited when we will have back end functionality
+		$.ajax({
+			url: "some_url_for_request",
+			type: "POST",
+			data: { searchWord: searchWord},
+			dataType: "json",
+			success: function(data) {
+				if(data) {
+					var searhResult = '';
+					for(let i = 0; i < data.length; i++) {
+						searchResult = '<a href="#" class="list-group-item list-group-item-action search-data">'+data['name']+'</li>'
+					}
+					$('.listSearch').html(searchResult);
+				} else {
+					$('.listSearch').html('<span class="list-group-item list-group-item-action">No result</span>');
+				}
+			},
+			error: function (xhr, status) {
+				console.log("Sorry, there was a problem!");
+			}
+		});
+		*/
 	});
 
 	/*function setEqualHeight(columns) {
--- layouts/header.php
+++ layouts/header.php
@@ -24,6 +24,32 @@
 										<span class="icon-search"></span>
 									</label>
 									<input type="text" class="form-control" id="search-input">
+									<div class="search-result">
+										<div class="scrollbar">
+											<div class="listSearch list-group">
+						                        <a href="#" class="list-group-item list-group-item-action">USDEUR</a>
+						                        <a href="#" class="list-group-item list-group-item-action">USDBTC</a>
+						                        <a href="#" class="list-group-item list-group-item-action">USDCAD</a>
+						                        <a href="#" class="list-group-item list-group-item-action">USDHRK</a>
+						                        <a href="#" class="list-group-item list-group-item-action">NEOSBTC</a>
+						                        <a href="#" class="list-group-item list-group-item-action">USDXRP</a>
+						                        <a href="#" class="list-group-item list-group-item-action">USDEEUR</a>
+						                        <a href="#" class="list-group-item list-group-item-action">ARSUSD</a>
+						                        <a href="#" class="list-group-item list-group-item-action">ARSCNY</a>
+						                        <a href="#" class="list-group-item list-group-item-action">GHSUSD</a>
+						                        <a href="#" class="list-group-item list-group-item-action">GHSCNY</a>
+						                        <a href="#" class="list-group-item list-group-item-action">ACESETH</a>
+						                        <a href="#" class="list-group-item list-group-item-action">ACESBTC</a>
+						                        <a href="#" class="list-group-item list-group-item-action">AGRSETH</a>
+						                        <a href="#" class="list-group-item list-group-item-action">AGRSBTC</a>
+						                        <a href="#" class="list-group-item list-group-item-action">ARS(Argentine Peso)</a>
+						                        <a href="#" class="list-group-item list-group-item-action">GHS(Ghana Cedi)</a>
+						                        <a href="#" class="list-group-item list-group-item-action">ACES(Aces)</a>
+						                        <a href="#" class="list-group-item list-group-item-action">AGRS(Agoras Tokens)</a>
+						                        <span style="display:none" class="list-group-item list-group-item-action no-result">No result</span>
+						                    </div>
+					                    </div>
+									</div>
 								</div>
 							</form>
 						</li>
